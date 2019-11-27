8c186283db6b1840668a7633a37b295a83130399
hellochan <284910889@qq.com>
2019-11-27 12:00:28 +0800

    init commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.kohsuke.github.GitHub;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@SpringBootTest
+class DemoApplicationTests {
+    private static final String username = "";
+    private static final String password = "";
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void authentication() throws Exception{
+        GitHub gitHub = GitHub.connectUsingPassword("username", "password");
+    }
+
+}
