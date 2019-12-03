67b31c8aa5b6aa33da81c3be8b44eb71bf59c195
hellochan <284910889@qq.com>
2019-12-03 20:10:24 +0800

    init commit

---
+++ src/main/java/com/example/demo/test/MonitorTest.java
@@ -0,0 +1,17 @@
+package com.example.demo.test;
+
+/**
+ * <pre>
+ * 程序的中文名称。
+ * </pre>
+ *
+ * @author chenxinhuan  chenxinhuan@kungeek.com
+ * @version 1.00.00
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期:     修改内容:
+ * </pre>
+ */
+public class MonitorTest {
+    private static final String KUNGEEK = "kungeek";
+}
